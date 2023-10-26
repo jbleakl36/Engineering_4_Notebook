0e411edb5bda5f65fe792c8fa5065a489e6d7a44
jbleakl36 <jbleakl36@charlottesvilleschools.org>
2023-10-26 10:19:14 -0400

    funnyier

--- raspberry-pi/LandingArea1.py
+++ raspberry-pi/LandingArea1.py
@@ -2,7 +2,7 @@
 def triangle_area(x1y1, x2y2, x3y3):
     try:
     
-        
+        #Establishing variables
         x1y1 = x1y1.split(",")
         x2y2 = x2y2.split(",")
         x3y3 = x3y3.split(",")
@@ -20,11 +20,11 @@ def triangle_area(x1y1, x2y2, x3y3):
         y2 = float(y2)
         x3 = float(x3)
         y3 = float(y3)
-
+# Calculation for area
         area=  abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2
 
         return area
-
+# Error message
     except:
         print("incompatable syntax detected, please try again >:(")
 
@@ -35,7 +35,7 @@ def triangle_area(x1y1, x2y2, x3y3):
 
 
 while True:
-
+# Prompt for the inputs
     val1 = input("Enter the first coordinate values of x,y: ")
     val2 = input("Enter the second coordinate values of x,y: ")
     val3 = input("Enter the first coordinate values of x,y: ")
@@ -47,7 +47,7 @@ while True:
         continue
 
 
-
+# Success message
     else:
         print(f"The area of the triangle with vertices {val1}, {val2}, {val3} is {area} square km")
 
